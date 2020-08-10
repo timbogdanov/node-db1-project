f155583769751c0b26cdcf1b883432c9cd977d31
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-08-10 13:35:16 -0700

    complete mvp

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
-const express = require("express");
+const express = require('express');
 
-const db = require("../data/dbConfig.js");
+const db = require('../data/dbConfig.js');
+const accountsRouter = require('../accounts/account-router');
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', accountsRouter);
 
 module.exports = server;
